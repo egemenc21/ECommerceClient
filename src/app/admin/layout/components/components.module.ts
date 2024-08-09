c2773e5603830a2bbbeb963216c1d710108a9f2c
Egemen <egemenc2101@gmail.com>
2024-08-09 13:15:06 +0300

    Configuration of Material UI: - Material UI added to the project and used in admin route - List component is added to the sidebar.component.html

--- src/app/admin/layout/components/components.module.ts
+++ src/app/admin/layout/components/components.module.ts
@@ -4,10 +4,11 @@ import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
+import { MatListModule} from "@angular/material/list";
 
 @NgModule({
   declarations: [HeaderComponent, SidebarComponent, FooterComponent],
   exports: [HeaderComponent, FooterComponent, SidebarComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatListModule],
 })
 export class ComponentsModule {}
--- src/app/admin/layout/layout.module.ts
+++ src/app/admin/layout/layout.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
-import {ComponentsModule} from "./components/components.module";
-import {RouterOutlet} from "@angular/router";
-
-
+import { ComponentsModule } from './components/components.module';
+import { RouterOutlet } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [LayoutComponent],
-  imports: [CommonModule, ComponentsModule, RouterOutlet],
+  imports: [CommonModule, ComponentsModule, RouterOutlet, MatSidenavModule],
   exports: [LayoutComponent],
 })
 export class LayoutModule {}
