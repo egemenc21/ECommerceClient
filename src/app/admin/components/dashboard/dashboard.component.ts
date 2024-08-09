1a06d1c1c2351840e8c4fcddfd13158c490170b9
Egemen <egemenc2101@gmail.com>
2024-08-09 15:57:06 +0300

    Added AlertifyService for message handling - Created AlertifyService for alert management using the alertify library. - Defined MessageType enum for message types (Error, Message, Notify, Success, Warning).
    Enhanced AlertifyService with new features
    - Added AlertifyOptions for configuration including delay, position, and dismissal.
    - Introduced Position enum for alert positions.
    - Updated message method and added dismiss method.

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  AlertifyService,
+  MessageType,
+  Position,
+} from '../../../services/admin/alertify.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  constructor(public alertify: AlertifyService) {}
 
+  ngOnInit() {}
+
+  m() {
+    this.alertify.message('mlala', {
+      messageType: MessageType.Warning,
+      dismissOthers: true,
+      delay: 2,
+      position: Position.BottomRight,
+    });
+  }
+  d() {
+    this.alertify.dismiss();
+  }
 }
